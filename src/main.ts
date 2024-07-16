fe4feb7f9f3ce37fb68c3489739259bb33e1c377
jguerinet <julien@guerinet.com>
2024-07-16 11:47:26 +0100

    Attempt to support folders

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import * as fs from 'fs'
-import * as os from 'os'
 import * as core from '@actions/core'
-import axios, { ResponseType } from 'axios'
+import axios, { AxiosResponse, ResponseType } from 'axios'
 
-function parseFileIdFromURL(path?: string) : string | undefined {
+type FileMetadata = {
+  name: string, 
+  id: string
+}
+
+function parseDriveIdFromURL(isFile: boolean, path?: string) : string | undefined {
   if (!path) {
     return undefined
   }
 
   // Ensure the file path matches a Google Drive path
-  const prefix = '/file/d/'
-  const suffix = '/view'
+  const prefix = isFile ? '/file/d/' : '/drive/folders/'
+  const suffix = isFile ? '/view' : ''
 
   const urlPath = new URL(path).pathname
   if (!urlPath.startsWith(prefix) || !urlPath.endsWith(suffix)) {
@@ -18,9 +22,13 @@ function parseFileIdFromURL(path?: string) : string | undefined {
     return undefined
   }
 
-  // Strip the prefix/suffix to get the file id
-  const fileId = urlPath.slice(prefix.length, -suffix.length)
-  return fileId
+  // Strip the prefix/suffix to get the Drive id
+  const driveId = urlPath.slice(prefix.length, -suffix.length)
+  return driveId
+}
+
+function getGoogleDriveUrl(driveId: string | undefined = undefined): string {
+  return `https://www.googleapis.com/drive/v3/files/${driveId}`
 }
 
 async function run() : Promise<void> {
@@ -33,9 +41,14 @@ async function run() : Promise<void> {
     return
   }
 
-  const fileId = core.getInput('file-id') || parseFileIdFromURL(core.getInput('file-url'))
-  if (!fileId) {
-    core.setFailed('Action could not determine file id')
+  const fileId = core.getInput('file-id')
+  const fileUrl = core.getInput('file-url')
+  const folderId = core.getInput('folder-id')
+  const folderUrl = core.getInput('folder-url')
+
+  if (!fileId && !fileUrl && !folderId && !folderUrl) {
+    // If none of them are defined, error out
+    core.setFailed('You must define one of following four inputs: fileId, fileUrl, folderId, folderUrl')
     return
   }
 
@@ -45,8 +58,63 @@ async function run() : Promise<void> {
     return
   }
 
+  const isFile = fileId || fileUrl
+  const fileIds: FileMetadata[] = []
+
+  if (isFile) {
+    // If we are downloading a file, parse the Id and add it to the array 
+    const finalFileId = fileId ?? parseDriveIdFromURL(true, fileUrl)
+    if (!finalFileId) {
+      core.setFailed('Action could not determine file id')
+      return
+    }
+    fileIds.push({name: path, id: finalFileId})
+  } else {
+    // If we are downloading a folder, parse the folder Id
+    const finalFolderId = folderId ?? parseDriveIdFromURL(false, folderUrl)
+    if (!finalFolderId) {
+      core.setFailed('Action could not determine folder id')
+      return
+    }
+    // Query Google Drive to get the list of files in the folder
+    const url = getGoogleDriveUrl()
+
+    const options = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        q: `${finalFolderId} in parents`,
+        supportsAllDrives: true,
+        pageSize: 1000
+      },
+    }
+
+    const response = await axios.get(url, options)
+    if (response.status != 200) {
+      core.setFailed(`Failed to list files in folder from Google drive: ${response.status}`)
+      return
+    }
+
+    // Add the file Ids to the array of files to download
+    response.data.files.forEach(file => fileIds.push({name: file.name, id: file.id}));
+  }
+
+  if (fileIds.length === 0) {
+    core.setFailed("Failed to find any files to download")
+  } else if (fileIds.length === 1) {
+    // Only 1 file to download
+    await downloadFile(token, fileIds[0])
+  } else {
+    // Create a folder at the path given 
+    fs.mkdirSync(path)
+    fileIds.forEach(async fileId => await downloadFile(token, fileId))
+  }
+}
+
+async function downloadFile(token: string, file: FileMetadata) {
   // Query Google Drive
-  const fileUrl = `https://www.googleapis.com/drive/v3/files/${fileId}`
+  const url = getGoogleDriveUrl(file.id)
 
   const options = {
     headers: {
@@ -60,27 +128,27 @@ async function run() : Promise<void> {
     responseType: 'stream' as ResponseType,
   }
 
-  const response = await axios.get(fileUrl, options)
+  const response = await axios.get(url, options)
   if (response.status != 200) {
     core.setFailed(`Failed to get file from Google drive: ${response.status}`)
     return
   }
-
-  core.saveState('path', path)
-
+  
   // Write file out
-  const file = fs.createWriteStream(path)
-  response.data.pipe(file)
+  core.saveState('path', file.name)
+
+  const downloadedFile = fs.createWriteStream(file.name)
+  response.data.pipe(downloadedFile)
 }
 
 async function post() : Promise<void> {
 
   // Remove the downloaded file
-  const path = core.getState('path')
-  if (path && fs.existsSync(path)) {
-    fs.rmSync(path)
-    console.log(`Removed downloaded file ${path}`)
-  }
+  // const path = core.getState('path')
+  // if (path && fs.existsSync(path)) {
+  //   fs.rmSync(path)
+  //   console.log(`Removed downloaded file ${path}`)
+  // }
 }
 
 if (!core.getState('isPost')) {
